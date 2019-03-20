3046c5a3c9faba14b6620565cde10551f5d5abe9
mtotodev05 <mtotodev05@gmail.com>
2019-03-20 20:00:24 +0300

    Setup routes

--- lib/app.ts
+++ lib/app.ts
@@ -1,12 +1,15 @@
 import * as express from "express"
 import * as bodyParser from "body-parser"
+import {Routes} from "./routes/routes"
 
 class App {
     public app: express.Application;
+    public routesProvider: Routes = new Routes()
 
     constructor() {
         this.app = express()
         this.config()
+        this.routesProvider.routes(this.app)
     }
 
     private config(): void {
