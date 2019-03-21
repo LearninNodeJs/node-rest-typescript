34ad533e885bd2e6175c88f656cc35484afe5476
mtotodev05 <mtotodev05@gmail.com>
2019-03-21 07:41:33 +0300

    Setup all routes

--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -8,5 +8,41 @@ export class Routes {
                     message: 'Get Working Successfully'
                 })
             })
+
+        app.route('contact')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'Contact Get is Successful'
+                })
+            })
+
+        app.route('/add')
+            .post((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'Post Request Successful'
+                })
+            })
+
+        app.route('/contact/:contactId')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'Get using Parameters successful'
+                })
+            })
+
+        app.route('/update')
+            .put((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'Put Request Successful'
+                })
+            })
+
+        app.route('/contact/:id')
+            .delete((req: Request,res: Response)=>{
+                res.status(200).send({
+                    message: 'Delete request successful'
+                })
+            })
+
     }
 }
