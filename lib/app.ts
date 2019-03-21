faa3ca9a500c558c15f0d39a45ce359cfbfff665
mtotodev05 <mtotodev05@gmail.com>
2019-03-21 07:30:27 +0300

    Fix bug in routes

--- lib/app.ts
+++ lib/app.ts
@@ -11,15 +11,14 @@ class App {
     constructor() {
         this.app = express()
         this.config()
-        this.routesProvider.routes()
-
+        this.routesProvider.routes(this.app)
     }
 
     private config(): void {
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({extended: false}))
         this.app.use(morgan('dev'))
-        this.app.use(cors)
+        this.app.use(cors())
     }
 }
 
--- lib/routes/routes.ts
+++ lib/routes/routes.ts
@@ -1,14 +1,12 @@
-import {Request,Response} from "express"
-import * as express from "express"
-
-let router = express.Router()
+import {Request, Response} from "express"
 
 export class Routes {
-    public  routes(): void {
-        router.get('/',(req : Request,res: Response)=>{
-            res.status(200).send({
-                message:'Message sent successfully'
+    public routes(app): void {
+        app.route('/')
+            .get((req: Request, res: Response) => {
+                res.status(200).send({
+                    message: 'Get Working Successfully'
+                })
             })
-        })
     }
 }
