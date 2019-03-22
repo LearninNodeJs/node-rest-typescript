4ef02355597e0fe4f6185bf7d75f2183d15aa625
mtotodev05 <mtotodev05@gmail.com>
2019-03-22 08:29:29 +0300

    Coneect database, and commit

--- lib/app.ts
+++ lib/app.ts
@@ -3,15 +3,21 @@ import * as bodyParser from "body-parser"
 import {Routes} from "./routes/routes"
 import * as morgan from "morgan"
 import * as cors from "cors"
+import *  as mongoose from "mongoose"
+
+require('dotenv').config()
 
 class App {
     public app: express.Application;
     public routesProvider: Routes = new Routes()
+    public mongoDatabaseURL: string = process.env.MONGO_DB
 
     constructor() {
         this.app = express()
         this.config()
         this.routesProvider.routes(this.app)
+        this.connectMongo()
+
     }
 
     private config(): void {
@@ -19,6 +25,17 @@ class App {
         this.app.use(bodyParser.urlencoded({extended: false}))
         this.app.use(morgan('dev'))
         this.app.use(cors())
+    }
+
+    private async connectMongo(): mongoose.connect {
+        let connection;
+        try {
+            connection = await mongoose.connect(this.mongoDatabaseURL,{useNewUrlParser:true})
+            console.log("Mongo Connected Successfully")
+        } catch (e) {
+            console.error("Failed to Load Mongoose: ",e.message)
+        }
+        return connection;
     }
 }
 
--- lib/controller/contact-controller.ts
+++ lib/controller/contact-controller.ts
@@ -7,10 +7,19 @@ export class ContactController {
     public async addNewContact(req: Request, res: Response) {
         try {
             let _this = req.body
-            let contact = new ContactSchema(_this)
+            let contact = new ContactSchema({
+                _id: new mongoose.Types.ObjectId(),
+                firstName:_this.firstName,
+                lastName: _this.lastName,
+                email: _this.email,
+                company: _this.company,
+                phone: _this.phone
+
+            })
             let saveContact = await contact.save()
             res.status(200).send({
-                message: 'Contact Saved Successfully'
+                message: 'Contact Saved Successfully',
+                saveContact
             })
         } catch (e) {
             res.status(500).send({
